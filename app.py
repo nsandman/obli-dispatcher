8fb41431893e68cdcf9db4bbb31bec2f8f7318e5
Noah Sandman <nsandman09@gmail.com>
2019-10-19 15:30:16 -0700

    Messenger API works, but needs a lot of cleanup

--- app.py
+++ app.py
@@ -1,104 +1,36 @@
 import sys
-import yaml
-import klepto
+import json
 import socketio
 
+import nest_asyncio
+import asyncio
+
 from sanic import Sanic
-from inspect import isclass
-from sanic_cors import CORS
-from pypika import Table, Query
-from sanic.response import text
-from sqlalchemy import create_engine
 from os import getenv, listdir, path, getcwd
 
-from response import Response
 # from urltask import UrlTask
 from constants import *
 
 sio = socketio.AsyncServer(async_mode="sanic", cors_allowed_origins=[])
 app = Sanic()
-app.config["CORS_SUPPORTS_CREDENTIALS"] = True
-CORS(app)
 sio.attach(app)
-
-mysql_prefs = {}
 
 @sio.event
 async def event(sid, params):
-    global mysql_prefs
+    loop = asyncio.get_event_loop()
+    statement = "output = loop.run_until_complete({module}.{name}(Interface('{sid}', '{module}')"
+    try:
+        if params["data"]:
+            statement += ', """{data}"""'
+    except:
+        pass
 
-    output = object()
+    statement = (statement + "))").format(**params,sid=sid)
+    exec(statement, globals())
 
-    statement = "output = {method}.{name}("
-    if params["data"]:
-        statement += "{data}"
-    statement = (statement + ")").format(params)
-    exec(statement)
-
-    if (output.response):
-        return output.response
-    else:
-        if output.mode is Response.MODE_TARGET:
-            if isinstance(output.data, string):
-                free_client_open(output.data)
-                return text("ok")
-        elif output.mode is Response.MODE_DBGET:
-            fetched_dict = {}
-
-            for (key, value) in output.data.items():
-                table_name = "_{0}_{1}".format(output.module, key)
-                mysql_u = gen_mysql_url(mysql_prefs, table_name)
-
-                fetched_dict[key] = {}
-
-                d = klepto.archives.sqltable_archive(mysql_u)
-                d.load()
-
-                engine = create_engine(mysql_u, echo=True)
-                conn = engine.connect()
-
-                keys_to_fetch = []
-                for item_key in value:
-                    if key["is_object"]:
-                        fetched_dict[key].update({item_key: d[item_key["name"]]})
-                        value.pop(item_key)
-                    else:
-                        keys_to_fetch.append(item_key["name"])
-
-                table = Table(table_name)
-                q = Query._from(table).select(*keys_to_fetch)
-
-                with conn.cursor() as cursor:
-                    cursor.execute(q)
-                    fetched_dict[key].update(cursor.fetchall())
-
-                del d
-                if output.cb:
-                    output.cb(fetched_dict)
-        elif output.mode is Response.MODE_DBSAVE:
-            for (key, value) in output.data.items():
-                table_name = "_{0}_{1}".format(output.module, key)
-                mysql_u = gen_mysql_url(mysql_prefs, table_name)
-
-                d = klepto.archives.sqltable_archive(mysql_u)
-                engine = create_engine(mysql_u, echo=True)
-                conn = engine.connect()
-
-                for row in value:
-                    for (col, m_val) in row.items():
-                        if isclass(m_val):
-                            d[col] = m_val
-                            del value[col]
-
-                    table = Table(table_name)
-
-                    sql_params = [Param("%s") for i in len(row)]
-                    q = Query.into(table).columns(*row.keys()).insert(sql_params)
-                    conn.execute(q, row.values())
-
-                d.dump()
-                conn.commit()
-                del d
+    if isinstance(output, (dict, list)):
+        return json.dumps(output)
+    return output
 
 connected = []
 @sio.event
@@ -141,6 +73,18 @@ async def free_client_open(url):
         pass
         # UrlTask(url)
 
+class Interface():
+    def __init__(self, sid, name):
+        self.sid  = sid
+        self.name = name
+
+    async def send(self, event, data):
+        await sio.emit("event", {
+            "event": event,
+            "data":  data,
+            "me": self.name
+        })
+
 if __name__ == "__main__":
     modules_dir = path.join(getcwd(), "modules")
     sys.path.append(modules_dir)
@@ -149,9 +93,13 @@ if __name__ == "__main__":
             print("Loading module", module)
             exec("import " + module)
 
-    with open("config.yaml") as f:
-        data = yaml.load(f, Loader=yaml.FullLoader)
-        mysql_prefs = data["mysql"]
+    # disable uvloop and replace the default
+    # event loop with a normal one so we 
+    # can nest coroutines
+    asyncio.set_event_loop_policy(None)
+    loop = asyncio.new_event_loop()
+    nest_asyncio.apply(loop)
+    asyncio.set_event_loop(loop)
 
     PORT = getenv("PORT", 8000)
     app.run(host="0.0.0.0", port=PORT)
